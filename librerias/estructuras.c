400e47cb945725a4658919a2cecb6d4b3e3266e4
BrickmanRodrigo <brickmanrodrigo@gmail.com>
2017-04-23 16:30:20 -0300

    FIX memcpy del send y agregado case del PCB para recv

--- librerias/estructuras.c
+++ librerias/estructuras.c
@@ -13,14 +13,15 @@
 
 #define HANDSHAKE 1
 #define MENSAJE 2
-
+#define PCB 3
 
 int serializeAndSend(t_msg* mensaje, int receiver){
 	int mallocSize = sizeof(mensaje->m_id) + sizeof(mensaje->m_size) + mensaje->m_size;
 	char* aux = malloc(mallocSize);
 
-
-	memcpy(aux,mensaje,sizeof mensaje);
+	memcpy(aux,&mensaje->m_id,sizeof(mensaje->m_id));
+	memcpy(aux + sizeof(mensaje->m_id),&mensaje->m_size,sizeof(mensaje->m_size));
+	memcpy(aux + sizeof(mensaje->m_id) + sizeof(mensaje->m_size), &mensaje->m_payload, sizeof(mensaje->m_payload));
 
 	send(receiver,aux,mallocSize,0);
 	//todo: Send "canchero" (Catcheando excepciones)
@@ -30,22 +31,36 @@ int serializeAndSend(t_msg* mensaje, int receiver){
 }
 
 int recvAndDeserialize(int sender, void* retrnVal){
+	int recvValue;
 	int id_buffer;
 	int size_buffer;
-	int pl_buffer;
-	recv(sender,&id_buffer,4,0);
-	switch(id_buffer){
-	case HANDSHAKE:
-		recv(sender,NULL,4,0);
-		recv(sender,&pl_buffer,4,0);
-		retrnVal = &pl_buffer;
-		break;
-	case MENSAJE:
-		recv(sender,&size_buffer,4,0);
-		char pl_buffer[size_buffer];
-		recv(sender,pl_buffer,size_buffer,0);
-		retrnVal = &pl_buffer;
-		break;
+	char* pl_buffer;
+	t_PCB* pcb_buffer = malloc(sizeof(t_PCB));
+	recvValue = recv(sender,&id_buffer,4,0);
+	if(recvValue != 0){
+		switch(id_buffer){
+		case HANDSHAKE:
+			recv(sender,&size_buffer,4,0);
+			pl_buffer = malloc(size_buffer);
+			recv(sender,&pl_buffer,4,0);
+			retrnVal = &pl_buffer;
+			break;
+		case MENSAJE:
+			recv(sender,&size_buffer,4,0);
+			pl_buffer = malloc(size_buffer);
+			recv(sender,&pl_buffer,size_buffer,0);
+			retrnVal = &pl_buffer;
+			break;
+		case PCB:
+			recv(sender,&size_buffer,4,0);
+			pl_buffer = malloc(size_buffer);
+			recv(sender,&pcb_buffer->PID,sizeof(pcb_buffer->PID),0);
+			recv(sender,&pcb_buffer->PC,sizeof(pcb_buffer->PC),0);
+			retrnVal = &pcb_buffer;
+			break;
+		}
+	}else{
+		return 0;
 	}
 	return 1;
 }
